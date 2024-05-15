67ea9f249315707f5eb2c2a826bad0bc51f73143
ESaulDJLaguna <esdjl_asse15@hotmail.com>
2024-05-15 15:26:54 -0500

    Fin sección 3

--- CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -1,8 +1,12 @@
 ﻿
 namespace CleanArchitecture.Domain.Common
 {
-	public class BaseDomainModel
+	public abstract class BaseDomainModel
 	{
-        public int Id { get; set; }
-    }
+		public int Id { get; set; }
+		public DateTime? DateCreated { get; set; }
+		public string? CreatedBy { get; set; }
+		public DateTime? LastModifiedDate { get; set; }
+		public string? LastModifiedBy { get; set; }
+	}
 }
